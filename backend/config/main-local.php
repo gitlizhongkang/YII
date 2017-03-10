aa55b9af8c2bb23ab573587bcce8958e8e867db2
lizhongkang <18303586720@163.com>
2017-03-10 16:50:18 +0800

    '2'

---
+++ backend/config/main-local.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    'components' => [
+        'request' => [
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => 'JrQ3CFD4vsY1Bw4r2mYwdvQ5AI9SLA5E',
+        ],
+    ],
+];
