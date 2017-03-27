51d37c334419452b5c054ef08e2cdd8d8f827fa2
maohongrui1 <mhrmaohongrui@sina.com>
2017-03-27 15:09:43 +0800

    mmmm327

--- frontend/views/layouts/header.php
+++ frontend/views/layouts/header.php
@@ -93,3 +93,12 @@ $user=$session->get('user');
     });
 </script>
     <?php echo $content; ?>
+        <div id="footer">
+        <div class="wrapper">
+            <a href="<?=Url::to(['index/about'])?>" target="_blank" rel="nofollow">联系我们</a>
+            <a href="h/af/zhaopin.html" target="_blank">互联网公司导航</a>
+            <a href="http://e.weibo.com/lagou720" target="_blank" rel="nofollow">拉勾微博</a>
+            <a class="footer_qr" href="javascript:void(0)" rel="nofollow">拉勾微信<i></i></a>
+            <div class="copyright">&copy;2013-2014 Lagou <a target="_blank" href="http://www.miitbeian.gov.cn/state/outPortal/loginPortal.action">京ICP备14023790号-2</a></div>
+        </div>
+    </div>
